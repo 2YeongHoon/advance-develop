7ed1464152ea5c1585af632c3afa2bbb5e47be69
yeonghoon.lee <djaakclsrn12@naver.com>
2024-03-09 13:51:44 +0900

    NEW: Initial commit

---
+++ src/main/java/com/advance/advancedevelop/AdvanceDevelopApplication.java
@@ -0,0 +1,13 @@
+package com.advance.advancedevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceDevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceDevelopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/advance/advancedevelop/AdvanceDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advance.advancedevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceDevelopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
