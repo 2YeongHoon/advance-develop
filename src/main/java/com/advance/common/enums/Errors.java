df0fe629f31ded84c99e05d27556364cdbc4814b
yeonghoon.lee <djaakclsrn12@naver.com>
2024-03-16 16:51:44 +0900

    NEW: 공통 예외 클래스 추가

---
+++ src/main/java/com/advance/common/enums/Errors.java
@@ -0,0 +1,18 @@
+package com.advance.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 오류 정의
+ */
+@Getter
+@AllArgsConstructor
+public enum Errors {
+    EXTERNAL_API_ERROR(1, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다"),
+    INTERNAL_SERVER_ERROR(9999, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다");
+
+    private final int code;
+    private final String message;
+
+}
---
+++ src/main/java/com/advance/common/exception/BaseRuntimeException.java
@@ -0,0 +1,30 @@
+package com.advance.common.exception;
+
+import com.advance.common.enums.Errors;
+import org.springframework.http.HttpStatus;
+
+public class BaseRuntimeException extends RuntimeException{
+
+    private static final Errors DEFAULT_ERROR = Errors.INTERNAL_SERVER_ERROR;
+    private static final HttpStatus DEFAULT_HTTP_STATUS = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    private final int errorCode;
+    private final String errorMessage;
+    private final int errorStatus;
+
+    public BaseRuntimeException(String message) {
+        super(message);
+
+        this.errorCode = DEFAULT_ERROR.getCode();
+        this.errorMessage = DEFAULT_ERROR.getMessage();
+        this.errorStatus = DEFAULT_HTTP_STATUS.value();
+    }
+    public BaseRuntimeException(Errors errors, HttpStatus httpStatus) {
+        super(errors.getMessage());
+
+        this.errorCode = errors.getCode();
+        this.errorMessage = errors.getMessage();
+        this.errorStatus = httpStatus.value();
+    }
+
+}
