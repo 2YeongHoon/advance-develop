13e21737d473262e783c7fe1a4b1e168e767322a
yeonghoon.lee <djaakclsrn12@naver.com>
2024-04-08 18:54:58 +0900

    NEW: 설정 변경 bean 추가

---
+++ src/main/java/com/advance/externalinjection/ExternalInjectionBean.java
@@ -0,0 +1,24 @@
+package com.advance.externalinjection;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@RefreshScope
+public class ExternalInjectionBean {
+
+    private String color;
+
+    public ExternalInjectionBean(@Value("${test}") String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
