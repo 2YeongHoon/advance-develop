c9422532e341f2cb44eb3bfb6a8fbf2ebd929041
yeonghoon.lee <djaakclsrn12@naver.com>
2024-04-10 15:35:22 +0900

    feat: 재시도 어노테이션 추가

---
+++ src/main/java/com/advance/retry/annotation/Retry.java
@@ -0,0 +1,13 @@
+package com.advance.retry.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Retry {
+    // 기본 재시도 회수
+    int value() default 3;
+}
