19d5d26e268af353fb6af83f5dee05abe3313ccb
2Yeonghoon <djaakclsrn12@naver.com>
2024-04-11 23:13:52 +0900

    NEW: 외부 빈 주입 테스트 컨트롤러 추가

---
+++ src/main/java/com/advance/externalinjection/controller/ExternalController.java
@@ -0,0 +1,12 @@
+package com.advance.externalinjection.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/external")
+public class ExternalController {
+
+}
