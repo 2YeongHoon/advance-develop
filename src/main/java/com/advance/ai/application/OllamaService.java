67b34982f59eb6ff72386cfee2a025f3aea0318d
yeonghoon.lee <djaakclsrn12@naver.com>
2024-05-30 22:25:57 +0900

    feat: 고정변수 상수화

--- src/main/java/com/advance/ai/application/OllamaService.java
+++ src/main/java/com/advance/ai/application/OllamaService.java
@@ -3,7 +3,6 @@ package com.advance.ai.application;
 import com.advance.ai.controller.dto.AiRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaApi.ChatRequest;
 import org.springframework.ai.ollama.api.OllamaApi.ChatResponse;
@@ -16,25 +15,29 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OllamaService {
 
-  private final OllamaChatClient chatClient;
+    // TODO llamaModle로 전환하기.
+    private static final String CONTENT = "한글로 대답해";
+    private static final String OLLAMA_API = "http://localhost:11434";
+    private static final String OLLAMA_VER = "llama3";
 
-  public ChatResponse test(AiRequest req) {
-    OllamaApi ollamaApi =
-        new OllamaApi("http://localhost:11434");
+    public ChatResponse sendSync(AiRequest req) {
+        OllamaApi ollamaApi =
+            new OllamaApi(OLLAMA_API);
 
-    // Sync request
-    var request = ChatRequest.builder("llama3")
-        .withStream(false) // not streaming
-        .withMessages(List.of(
-            Message.builder(Role.SYSTEM)
-                .withContent("You are geography teacher. You are talking to a student.")
-                .build(),
-            Message.builder(Role.USER)
-                .withContent(req.prom())
-                .build()))
-        .withOptions(OllamaOptions.create().withTemperature(0.9f))
-        .build();
+        // Sync request
+        var request = ChatRequest.builder(OLLAMA_VER)
+            .withStream(false)
+            .withMessages(List.of(
+                Message.builder(Role.SYSTEM)
+                    .withContent(CONTENT)
+                    .build(),
+                Message.builder(Role.USER)
+                    .withContent(req.prom())
+                    .build()))
+            .withOptions(OllamaOptions.create().withTemperature(0.9f))
+            .build();
 
-    return ollamaApi.chat(request);
-  }
+        return ollamaApi.chat(request);
+    }
+
 }
