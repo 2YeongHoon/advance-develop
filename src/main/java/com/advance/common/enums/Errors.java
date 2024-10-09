c295365359c4ce989312d045b030cdffec1cdae7
yeonghoon.lee <djaakclsrn12@naver.com>
2024-10-09 17:21:49 +0900

    feat: 메일 발송 실패 예외 추가

--- src/main/java/com/advance/common/enums/Errors.java
+++ src/main/java/com/advance/common/enums/Errors.java
@@ -10,6 +10,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Errors {
     EXTERNAL_API_ERROR(1, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다"),
+
+    NOT_FOUND_MAIL_SEND_TYPE(4001, "존재하지 않는 메일 발송 방식입니다."),
+
     INTERNAL_SERVER_ERROR(9999, "요청을 처리하던 중 예상하지 못한 오류가 발생했습니다");
 
     private final int code;
