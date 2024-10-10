56e70023de692f8397a67f234e4f574f58c2061c
yeonghoon.lee <djaakclsrn12@naver.com>
2024-10-10 18:42:24 +0900

    feat: redis 의존성 및 설정 추가

---
+++ src/main/java/com/advance/redis/service/RedisService.java
@@ -0,0 +1,14 @@
+package com.advance.redis.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+
+    private final StringRedisTemplate redisTemplate;
+
+}
+
