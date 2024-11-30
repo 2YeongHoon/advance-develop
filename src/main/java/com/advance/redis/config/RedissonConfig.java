3eba1c985fe32a68368a6a42de578ef64b33bd5f
yeonghoon.lee <djaakclsrn12@naver.com>
2024-11-30 23:47:50 +0900

    feat: redisson 설정 추가

---
+++ src/main/java/com/advance/redis/config/RedissonConfig.java
@@ -0,0 +1,22 @@
+package com.advance.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + "localhost:6379");
+        return Redisson.create(config);
+    }
+
+
+}
