0160d2bff42e3cbb815c71440db8fecea0c17ffe
yeonghoon.lee <djaakclsrn12@naver.com>
2024-12-05 18:59:43 +0900

    feat: AOP 트랜잭션 분리 클래스 추가

---
+++ src/main/java/com/advance/redis/aop/AopForTransaction.java
@@ -0,0 +1,19 @@
+package com.advance.redis.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * AOP에서 트랜잭션 분리를 위한 클래스
+ */
+@Component
+public class AopForTransaction {
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Object proceed(final ProceedingJoinPoint joinPoint) throws Throwable {
+        return joinPoint.proceed();
+    }
+
+}
