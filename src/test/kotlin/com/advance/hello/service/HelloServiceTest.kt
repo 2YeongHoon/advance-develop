627b319580cc3ad805fc0c619b603d6d1c42c6c4
yeonghoon.lee <djaakclsrn12@naver.com>
2025-05-10 23:50:20 +0900

    test: 코틀린 테스트 코드 추가

---
+++ src/test/kotlin/com/advance/hello/service/HelloServiceTest.kt
@@ -0,0 +1,12 @@
+package com.advance.hello.service
+
+import org.junit.jupiter.api.Test
+
+class HelloServiceTest {
+
+    @Test
+    fun `코틀린 문법 익히기`() {
+        println("hello world!!");
+    }
+
+}
